8d828d0a154488165524edda781320f37ab881dc
david <david@Mars>
2025-08-27 22:19:08 +0100

    Bugfix Fixed warning for dateutil and pkg_config

--- b3/pkg_handler.py
+++ b3/pkg_handler.py
@@ -38,20 +38,3 @@ def resource_directory(module):
     Use this if pkg_resources is NOT installed
     """
     return os.path.dirname(sys.modules[module].__file__)
-
-
-if not main_is_frozen():
-    try:
-        import pkg_resources
-    except ImportError:
-        pkg_resources = None
-        pass
-    else:
-        # package tools is installed
-        def resource_directory_from_pkg_resources(module):
-            """
-            Use this if pkg_resources is installed
-            """
-            return pkg_resources.resource_filename(module, '')
-
-        resource_directory = resource_directory_from_pkg_resources

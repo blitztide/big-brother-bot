5e5b60f6cf451691fdb81d40342906f5bdb0aeba
david <david@Mars>
2025-09-02 12:15:39 +0100

    Added test for path resolution

--- tests/core/test_b3.py
+++ tests/core/test_b3.py
@@ -25,6 +25,7 @@
 from __future__ import absolute_import
 from tests import B3TestCase
 import b3
+import os
     
 class Test_getConfPath(B3TestCase):
 
@@ -33,3 +34,10 @@ class Test_getConfPath(B3TestCase):
         self.assertEqual('/some/where/conf', b3.getConfPath(conf=self.console.config))
         self.console.config.fileName = "./b3.xml"
         self.assertEqual('.', b3.getConfPath(conf=self.console.config))
+
+    def test_pathResolution(self):
+        cwd = os.getcwd()
+        self.assertEqual(os.path.join(cwd,"b3","conf", "plugin_admin.ini"), b3.getAbsolutePath("@b3/conf/plugin_admin.ini"))
+        self.assertEqual(os.path.join(cwd,"b3","conf", "plugin_admin.ini"), b3.getAbsolutePath("@conf/plugin_admin.ini"))
+        homedir = os.path.expanduser("~")
+        self.assertEqual(os.path.join(homedir, ".b3"), b3.getAbsolutePath("@home/"))
